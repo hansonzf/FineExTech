9f16e5a483ecc9963687a51ad8fd9f312040b45a
Hanson <zhang_hanson@163.com>
2022-11-04 14:07:01 +0800

    1

--- Shippment.Domain/AggregateModels/TransportOrderAggregate/TransportCargo.cs
+++ Shippment.Domain/AggregateModels/TransportOrderAggregate/TransportCargo.cs
@@ -16,9 +16,3 @@ namespace Shippment.Domain.AggregateModels.TransportOrderAggregate
         public Cargo CargoInfo { get; protected set; }
     }
 }
-
-
-// Barcode为什么不定义在CargoInfo里面，有如下理由：
-//  1，barcode不是货物的概念，其主要作用是为了系统中可以标识一件货物
-//  2，barcode一般是由单独的服务分发，便于扫描枪扫码
-// 所以TransportCargo的构造函数需要接收外部传进来的barcode
--- Warehouse.Api/Models/CreateStorehousePayload.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Warehouse.Api.Models
-{
-    public class CreateStorehousePayload
-    {
-        public int TenantId { get; set; }
-        public long MerchantId { get; set; }
-        public string StorehouseName { get; set; }
-    }
-}
